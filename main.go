7f212176efe1512f205ab324f06caccd0efb5aa3
Abbos Yulchiev <abbosyulchiev1@gmail.com>
2023-11-25 19:26:16 +0500

    getting input from user and using it with fmt.Scan() method

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World!")
+	fmt.Println("Hello World!")
+
+	var firstname, lastname string
+	fmt.Scan(&firstname, &lastname)
+
+	fmt.Printf("Hello %s %s, welcome to the first GO program", firstname, lastname)
 }
